f938a32d5257da83da244680d98200cee98d9ea4
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-07-22 23:49:31 +0400

    Home component stub

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './Home/Home';
+import './App.scss';
+
+ReactDOM.render(<Home />, document.getElementById('app'));
---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './Home.scss';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className={styles.home}>
+        home
+      </div>
+    );
+  }
+}
